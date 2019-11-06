198df7ec6ed4dccee03e8d0e1ab2f846c5bd6230
maddisikorski <45678844+maddisikorski@users.noreply.github.com>
2019-11-06 14:22:27 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -41,19 +41,39 @@ int main( )
 
     Deck deck;
 
-    int numCards = 5;
-    Player p1("Joe");
-    Player p2("Jane");
+    int numCards = 7;
+    Player p1("Sarah");
+    Player p2("Maddi");
     deck.shuffle();
     dealHand(deck, p1, numCards);
     dealHand(deck, p2, numCards);
     cout << p1.getName() <<" has : " << p1.showHand() << endl;
     cout << p2.getName() <<" has : " << p2.showHand() << endl;
 
+    Card check1;
+    Card check2;
+
+    while(p1.checkHandForBook(check1, check2)){
+        p1.bookCards(check1, check2);
+        p1.removeCardFromHand(check1);
+        p1.removeCardFromHand(check2);
+    }
+    while(p2.checkHandForBook(check1, check2)){
+        p2.bookCards(check1, check2);
+        p2.removeCardFromHand(check1);
+        p2.removeCardFromHand(check2);
+    }
+
+    cout << p1.getName() <<" has : " << p1.showHand() << endl;
+    cout << p2.getName() <<" has : " << p2.showHand() << endl;
+
+    cout << p1.getName() <<" booked : " << p1.showBooks() << endl;
+    cout << p2.getName() <<" booked : " << p2.showBooks() << endl;
+
     Card chosenCard;
     int rankChosenCard;
     string rankStringChosenCard;
-    string cardToBeRemoved;
+    Card cardToBeRemoved;
     int goFish=0;
     while(deck.size()!=0) {
 
@@ -65,10 +85,26 @@ int main( )
             cout << p1.getName() << " asks- Do you have a " << rankStringChosenCard << endl;
 
             if (p2.rankInHand(chosenCard)) {
-                cout << p2.getName() << "says - Yes. I have a " << rankStringChosenCard << "." << endl;
+                cout << p2.getName() << " says - Yes. I have a " << rankStringChosenCard << "." << endl;
+                Card spade(chosenCard.getRank(), Card::spades);
+                Card heart(chosenCard.getRank(), Card::hearts);
+                Card club(chosenCard.getRank(), Card::clubs);
+                Card diamond(chosenCard.getRank(), Card::diamonds);
 
-                //p2.removeCardFromHand(chosenCard);
-                //p1.addCard(chosenCard);
+                if(p2.cardInHand(spade)){
+                    cardToBeRemoved=p2.removeCardFromHand(spade);
+                }
+                else if(p2.cardInHand(heart)){
+                    cardToBeRemoved=p1.removeCardFromHand(heart);
+                }
+                else if(p2.cardInHand(club)){
+                    cardToBeRemoved=p1.removeCardFromHand(club);
+                }
+                else if(p2.cardInHand(diamond)){
+                    cardToBeRemoved=p1.removeCardFromHand(diamond);
+                }
+
+                p1.addCard(cardToBeRemoved);
             }
             else {
                 cout << p2.getName() << " says- Go Fish" << endl;
@@ -76,6 +112,20 @@ int main( )
                 goFish=1;
                 cout<<p1.showHand()<<endl;
             }
+            while(p1.checkHandForBook(check1, check2)){
+                p1.bookCards(check1, check2);
+                p1.removeCardFromHand(check1);
+                p1.removeCardFromHand(check2);
+            }
+            if(p1.getHandSize() == 0){
+                p1.addCard(deck.dealCard());
+                goFish=1;
+            }
+            cout << p1.getName() <<" has : " << p1.showHand() << endl;
+            cout << p2.getName() <<" has : " << p2.showHand() << endl;
+
+            cout << p1.getName() <<" booked : " << p1.showBooks() << endl;
+            cout << p2.getName() <<" booked : " << p2.showBooks() << endl;
         }
         while(goFish!=0) {
 
@@ -86,9 +136,27 @@ int main( )
             cout << p2.getName() << " asks- Do you have a " << rankStringChosenCard << endl;
 
             if (p1.rankInHand(chosenCard)) {
-                cout << p1.getName() << "says - Yes. I have a " << rankStringChosenCard << "." << endl;
-                //p2.removeCardFromHand(chosenCard);
-                //p1.addCard(chosenCard);
+                cout << p1.getName() << " says - Yes. I have a " << rankStringChosenCard << "." << endl;
+
+                Card spade(chosenCard.getRank(), Card::spades);
+                Card heart(chosenCard.getRank(), Card::hearts);
+                Card club(chosenCard.getRank(), Card::clubs);
+                Card diamond(chosenCard.getRank(), Card::diamonds);
+
+                if(p1.cardInHand(spade)){
+                    cardToBeRemoved=p1.removeCardFromHand(spade);
+                }
+                else if(p1.cardInHand(heart)){
+                    cardToBeRemoved=p1.removeCardFromHand(heart);
+                }
+               else if(p1.cardInHand(club)){
+                    cardToBeRemoved=p1.removeCardFromHand(club);
+                }
+               else if(p1.cardInHand(diamond)){
+                    cardToBeRemoved=p1.removeCardFromHand(diamond);
+                }
+
+                p2.addCard(cardToBeRemoved);
             }
             else {
                 cout << p1.getName() << " says- Go Fish" << endl;
@@ -96,8 +164,33 @@ int main( )
                 goFish=0;
                 cout<<p2.showHand()<<endl;
             }
+            while(p2.checkHandForBook(check1, check2)){
+                p2.bookCards(check1, check2);
+                p2.removeCardFromHand(check1);
+                p2.removeCardFromHand(check2);
+            }
+            if(p2.getHandSize() == 0){
+                p2.addCard(deck.dealCard());
+                goFish=0;
+            }
+            cout << p1.getName() <<" has : " << p1.showHand() << endl;
+            cout << p2.getName() <<" has : " << p2.showHand() << endl;
+
+            cout << p1.getName() <<" booked : " << p1.showBooks() << endl;
+            cout << p2.getName() <<" booked : " << p2.showBooks() << endl;
         }
 
+    }
+
+
+    if(p1.getBookSize()>p2.getBookSize()){
+        cout << p1.getName() << "Wins!"<<endl;
+    }
+    else if(p1.getBookSize()<p2.getBookSize()){
+        cout << p2.getName() << "Wins!"<<endl;
+    }
+    else{
+        cout << "DRAW!" <<endl;
     }
     return EXIT_SUCCESS;
 }
